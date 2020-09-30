9e2d7939274dde12fc28cd7fc337afc866c7b117
Gadgetmies <gadgetmies@gmail.com>
2020-09-30 20:03:03 +0300

    Remove duplicate margin definition

--- front/src/Tracks.js
+++ front/src/Tracks.js
@@ -276,7 +276,7 @@ class Tracks extends Component {
                   this.setState({ updatingTracks: false })
                 }
               }}
-              style={{ margin: 2, height: '100%', width: 150, margin: 4 }}
+              style={{ height: '100%', width: 150, margin: 4 }}
               label={'Update list'}
               loadingLabel={'Updating list'}
             />
