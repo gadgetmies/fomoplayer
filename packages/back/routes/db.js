1106561e9e5d13acc83715ca06af4100d666c812
Gadgetmies <gadgetmies@gmail.com>
2021-04-04 21:15:35 +0300

    Take NULL values into account when selecting the best preview

--- packages/back/routes/db.js
+++ packages/back/routes/db.js
@@ -5,6 +5,7 @@ const pg = require('../db/pg.js')
 module.exports.getLongestPreviewForTrack = (id, format, skip) =>
   pg
     .queryRowsAsync(
+      // language=PostgreSQL
       sql`
     SELECT store__track_id AS "storeTrackId" , lower(store_name) AS "storeCode"
     FROM
@@ -12,7 +13,7 @@ module.exports.getLongestPreviewForTrack = (id, format, skip) =>
       store__track  NATURAL JOIN
       store
     WHERE track_id = ${id} AND store__track_preview_format = ${format}
-    ORDER BY store__track_preview_end_ms - store__track_preview_start_ms DESC
+    ORDER BY store__track_preview_end_ms - store__track_preview_start_ms DESC NULLS LAST
     OFFSET ${skip}
     LIMIT 1;
     `
