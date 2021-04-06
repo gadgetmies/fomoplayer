90cf007dabe752205c7e2497e24ed59cd7418d8b
Gadgetmies <gadgetmies@gmail.com>
2021-04-06 18:10:30 +0300

    Store Spotify refresh token in order for the server to refresh access token

--- packages/back/routes/shared/spotify.js
+++ packages/back/routes/shared/spotify.js
@@ -6,11 +6,8 @@ const spotifyApi = new SpotifyWebApi({
 })
 
 spotifyApi.clientCredentialsGrant().then(function(data) {
-  console.log('The access token expires in ' + data.body['expires_in'])
-  console.log('The access token is ' + data.body['access_token'])
-
-  // Save the access token so that it's used in future calls
   spotifyApi.setAccessToken(data.body['access_token'])
+  spotifyApi.setRefreshToken(data.body['refresh_token'])
 })
 
 module.exports = spotifyApi
