998c25b07b95c8e53c477b1286641dc0abb3684a
Gadgetmies <gadgetmies@gmail.com>
2021-07-01 16:39:04 +0300

    Sort artists and labels by following and name

---
+++ packages/back/migrations/sqls/20210701133658-order-labels-and-artists-by-following-and-name-up.sql
@@ -0,0 +1,229 @@
+CREATE OR REPLACE FUNCTION track_details(track_ids integer[], meta_account_user_id_in integer)
+  returns TABLE
+          (
+            track_id integer,
+            title    text,
+            heard    timestamp with time zone,
+            duration integer,
+            added    timestamp with time zone,
+            artists  json,
+            version  text,
+            labels   json,
+            remixers json,
+            releases json,
+            keys     json,
+            previews json,
+            stores   json,
+            released date
+          )
+  language sql
+as
+$$
+WITH
+  limited_tracks AS (
+    SELECT
+      track_id
+    FROM unnest(track_ids) AS track_id
+  )
+, keys AS (
+  SELECT
+    lt.track_id
+  , json_agg(json_build_object(
+      'system', key_system_code,
+      'key', key_name
+    )) AS keys
+  FROM
+    limited_tracks lt
+    NATURAL JOIN track__key
+    NATURAL JOIN key_system
+    NATURAL JOIN key_name
+  GROUP BY 1
+)
+, authors AS (
+  WITH unique_authors AS (
+    SELECT
+      DISTINCT ON (track_id, artist_id)
+      lt.track_id,
+      artist_id,
+      artist_name,
+      coalesce(bool_or(meta_account_user_id = meta_account_user_id_in), false) AS following
+    FROM
+      limited_tracks lt
+      JOIN track__artist ta ON (ta.track_id = lt.track_id AND ta.track__artist_role = 'author')
+      NATURAL JOIN artist
+      NATURAL LEFT JOIN store__artist
+      NATURAL LEFT JOIN store__artist_watch
+      NATURAL LEFT JOIN store__artist_watch__user
+    GROUP BY 1, 2, 3
+  )
+  SELECT
+    track_id
+  , json_agg(
+        json_build_object('name', artist_name, 'id', artist_id, 'following', following)
+        ORDER BY following DESC, artist_name
+      ) AS authors
+  FROM
+    unique_authors
+  GROUP BY 1
+)
+, remixers AS (
+  WITH unique_remixers AS (
+    SELECT
+      DISTINCT ON (lt.track_id, artist_id)
+      lt.track_id
+    , artist_id
+    , artist_name
+    , coalesce(bool_or(meta_account_user_id = meta_account_user_id_in), false) AS following
+    FROM
+      limited_tracks lt
+      JOIN track__artist ta
+           ON (ta.track_id = lt.track_id AND ta.track__artist_role = 'remixer')
+      NATURAL JOIN artist
+      NATURAL LEFT JOIN store__artist
+      NATURAL LEFT JOIN store__artist_watch
+      NATURAL LEFT JOIN store__artist_watch__user
+    GROUP BY 1, 2, 3
+  )
+  SELECT
+    track_id
+  , json_agg(
+        json_build_object('name', artist_name, 'id', artist_id, 'following', following)
+        ORDER BY following DESC, artist_name
+      ) AS remixers
+  FROM unique_remixers
+  GROUP BY 1
+)
+, previews AS (
+  SELECT
+    lt.track_id
+  , json_agg(
+        json_build_object(
+            'id', store__track_preview_id,
+            'format', store__track_preview_format,
+            'store', lower(store_name),
+            'url', store__track_preview_url,
+            'waveform', store__track_preview_waveform_url,
+            'length_ms', store__track_preview_end_ms - store__track_preview_start_ms,
+            'start_ms', store__track_preview_start_ms,
+            'end_ms', store__track_preview_end_ms
+          )
+        ORDER BY store__track_preview_end_ms - store__track_preview_start_ms DESC NULLS LAST
+      ) AS previews
+  FROM
+    limited_tracks lt
+    NATURAL JOIN store__track
+    NATURAL JOIN store__track_preview
+    NATURAL LEFT JOIN store__track_preview_waveform
+    NATURAL JOIN store
+  GROUP BY 1
+)
+, store_tracks AS (
+  SELECT distinct on (lt.track_id, store_id)
+    track_id
+  , store_id
+  , store__track_id
+  , store__track_released
+  , store__track_url
+  , store_name
+  , store__track_store_id
+  , store__release_url
+  FROM
+    limited_tracks lt
+    NATURAL JOIN store__track
+    NATURAL JOIN store
+    NATURAL LEFT JOIN release__track
+    NATURAL LEFT JOIN release
+    NATURAL LEFT JOIN store__release
+)
+, stores AS (
+  SELECT
+    track_id
+  , min(store__track_released) as release_date
+  , json_agg(
+        json_build_object(
+            'name', store_name,
+            'code', lower(store_name),
+            'id', store_id,
+            'trackId', store__track_store_id,
+            'url', store__track_url,
+            'release', json_build_object('url', store__release_url)
+          )
+      )                        AS stores
+  FROM store_tracks
+  GROUP BY 1
+)
+, labels AS (
+  WITH unique_labels AS (
+    SELECT DISTINCT ON (track_id, label_id)
+      track_id
+    , label_id
+    , label_name
+    , coalesce(bool_or(meta_account_user_id = meta_account_user_id_in), false) AS following
+    FROM
+      limited_tracks
+      NATURAL JOIN track__label
+      NATURAL JOIN label
+      NATURAL JOIN store__label
+      NATURAL LEFT JOIN store__label_watch
+      NATURAL LEFT JOIN store__label_watch__user
+    GROUP BY 1, 2, 3
+  )
+  SELECT
+    track_id
+  , json_agg(
+        json_build_object('name', label_name, 'id', label_id, 'following', following)
+        ORDER BY following DESC, label_name
+      ) AS labels
+  FROM unique_labels
+  GROUP BY 1
+)
+, releases AS (
+  SELECT
+    track_id,
+    json_agg(
+        json_build_object('id', release_id, 'name', release_name)
+      ) AS releases
+  FROM limited_tracks
+       NATURAL JOIN release__track
+       NATURAL JOIN release
+  GROUP BY 1
+)
+SELECT
+  track_id
+, track_title                  AS title
+, user__track_heard            AS heard
+, track_duration_ms            AS duration
+, track_added                  AS added
+, authors.authors              AS artists
+, track_version                AS version
+, CASE
+    WHEN labels.labels IS NULL
+      THEN '[]' :: JSON
+    ELSE labels.labels END     AS labels
+, CASE
+    WHEN remixers.remixers IS NULL
+      THEN '[]' :: JSON
+    ELSE remixers.remixers END AS remixers
+, CASE
+    WHEN releases.releases IS NULL
+      THEN '[]' :: JSON
+    ELSE releases.releases END AS releases
+, CASE
+    WHEN keys.keys IS NULL
+      THEN '[]' :: JSON
+    ELSE keys.keys END         AS keys
+, previews.previews            as previews
+, stores.stores
+, stores.release_date          AS released
+FROM
+  limited_tracks
+  NATURAL JOIN track
+  NATURAL JOIN user__track
+  NATURAL JOIN authors
+  NATURAL JOIN previews
+  NATURAL JOIN stores
+  NATURAL LEFT JOIN labels
+  NATURAL LEFT JOIN remixers
+  NATURAL LEFT JOIN releases
+  NATURAL LEFT JOIN keys
+$$
