3114e6618c49bfdc95e537d984b28240b6d57cbe
Gadgetmies <gadgetmies@gmail.com>
2021-08-21 13:07:42 +0300

    Do not pass showIcon from defaultProps to DOM in ExternalLink;
    Also clean up the implementation.

--- packages/front/src/ExternalLink.js
+++ packages/front/src/ExternalLink.js
@@ -3,15 +3,19 @@ import * as R from 'ramda'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class ExternalLink extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   static defaultProps = { showIcon: true }
+
   render() {
     return (
       <a
-        onClick={(e) => e.stopPropagation()}
-        {...this.props}
+        onClick={e => e.stopPropagation()}
         className={`${this.props.className || ''} 'external-link' link`}
         target="_blank"
-        {...R.dissoc('children', this.props)}
+        {...R.omit(['children', 'showIcon'], this.props)}
       >
         {this.props.children}
         &nbsp;
