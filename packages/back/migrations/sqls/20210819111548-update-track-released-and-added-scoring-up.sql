5ff5ca9ad593bd2bc720a403d035f005cf1910fb
Gadgetmies <gadgetmies@gmail.com>
2021-08-21 11:51:08 +0300

    Limit track_date_added_score to only positive values;
    Also simplify the track_date_released_score

--- packages/back/migrations/sqls/20210819111548-update-track-released-and-added-scoring-up.sql
+++ packages/back/migrations/sqls/20210819111548-update-track-released-and-added-scoring-up.sql
@@ -1,7 +1,7 @@
 DROP MATERIALIZED VIEW track_date_released_score;
 CREATE MATERIALIZED VIEW track_date_released_score AS
 SELECT track_id,
-       GREATEST(0, 60 - DATE_PART('days', NOW() - LEAST(NOW(), MIN(store__track_released)))) AS score
+       60 - LEAST(60, DATE_PART('days', NOW() - LEAST(NOW(), MIN(store__track_released)))) AS score
 FROM track
          NATURAL JOIN store__track
 GROUP BY track_id;
@@ -9,7 +9,7 @@ REFRESH MATERIALIZED VIEW track_date_released_score;
 
 DROP MATERIALIZED VIEW track_date_added_score;
 CREATE MATERIALIZED VIEW track_date_added_score AS
-SELECT track_id, 60 - GREATEST(0, DATE_PART('days', NOW() - LEAST(NOW(), track_added))) AS score
+SELECT track_id, 60 - LEAST(60, DATE_PART('days', NOW() - LEAST(NOW(), track_added))) AS score
 FROM track;
 REFRESH MATERIALIZED VIEW track_date_added_score;
 
