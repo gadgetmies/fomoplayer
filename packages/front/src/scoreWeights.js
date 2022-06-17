6754afe2f69f4a58b0a07bcb647f54660e428ce0
Gadgetmies <gadgetmies@gmail.com>
2022-06-18 01:05:28 +0300

    Adjust score weight steps;
    The step for the label purchase was too big and made the adjustment useless.

--- packages/front/src/scoreWeights.js
+++ packages/front/src/scoreWeights.js
@@ -1,8 +1,14 @@
 const scoreWeights = {
-  artist: { label: 'Artist purchases', min: 0, max: 10, step: 1, unit: 'points added  per purchased tracks by artist' },
-  label: { label: 'Label purchases', min: 0, max: 10, step: 1, unit: 'points added per purchased tracks by label' },
-  artist_follow: { label: 'Artist follow', min: 0, max: 10, step: 1, unit: 'points added per followed artists' },
-  label_follow: { label: 'Label follow', min: 0, max: 10, step: 1, unit: 'points added per followed labels' },
+  artist: {
+    label: 'Artist purchases',
+    min: 0,
+    max: 10,
+    step: 0.1,
+    unit: 'points added  per purchased tracks by artist'
+  },
+  label: { label: 'Label purchases', min: 0, max: 10, step: 0.1, unit: 'points added per purchased tracks by label' },
+  artist_follow: { label: 'Artist follow', min: 0, max: 10, step: 0.1, unit: 'points added per followed artists' },
+  label_follow: { label: 'Label follow', min: 0, max: 10, step: 0.1, unit: 'points added per followed labels' },
   date_released: {
     label: 'Date released',
     min: 0,
