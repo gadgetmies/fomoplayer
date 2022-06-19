ddc2bed88d61a4db3d790e41c8b67f3b88199070
Gadgetmies <gadgetmies@gmail.com>
2022-06-19 17:09:09 +0300

    Implement a mock route for running jobs

--- packages/back/routes/mock/api.js
+++ packages/back/routes/mock/api.js
@@ -1,9 +1,17 @@
 const logger = require('../../logger')(__filename)
 const router = require('express-promise-router')()
+const jobScheduling = require('../../job-scheduling')
 
 router.use('/email/*', ({ headers, body }, res) => {
   logger.debug('Received email sending request', { headers, body })
   res.send({})
+})
+
+router.post('/job/:jobId', ({ params: { jobName } }, res) => {
+  runJob(jobName).then(result => {
+    console.debug(`Finished job ${jobName}`, result)
+  })
+  res.status(204).send()
 })
 
 module.exports = router
