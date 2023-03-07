a97976ccc7b642e99f2f513100a56906c68910b0
Gadgetmies <gadgetmies@gmail.com>
2023-03-07 12:33:59 +0200

    Add Telegram bot logger

--- packages/back/logger.js
+++ packages/back/logger.js
@@ -1,18 +1,29 @@
 const winston = require('winston')
-const { combine, printf } = winston.format
+const { combine, timestamp, printf, colorize, align } = winston.format
+const TelegramLogger = require('winston-telegram')
+
+let transports = [new winston.transports.Console()]
+if (process.env.TELEGRAM_BOT_TOKEN) {
+  transports.push(
+    new TelegramLogger({
+      name: 'error-channel',
+      token: process.env.TELEGRAM_BOT_TOKEN,
+      chatId: process.env.TELEGRAM_BOT_CHAT_ID,
+      level: 'error',
+      unique: true
+    })
+  )
+}
 
 const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      level: process.env.LOG_LEVEL || 'info',
-      format: combine(
-        printf(
-          ({ timestamp, moduleName, level, message, ...meta }) =>
-            `${level} [${moduleName.replace(require.main.path, '')}]: ${message}, meta: ${JSON.stringify(meta)}`
-        )
-      )
-    })
-  ]
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(
+    printf(
+      ({ timestamp, moduleName, level, message, ...meta }) =>
+        `${level} [${moduleName.replace(require.main.path, '')}]: ${message}, meta: ${JSON.stringify(meta)}`
+    )
+  ),
+  transports
 })
 
 module.exports = function(name) {
