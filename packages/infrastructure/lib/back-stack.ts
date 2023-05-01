ece034af916e4e2f555768d193a3b08adbe58ed5
Gadgetmies <gadgetmies@gmail.com>
2023-05-01 22:30:12 +0300

    Add Fargate

--- packages/infrastructure/lib/back-stack.ts
+++ packages/infrastructure/lib/back-stack.ts
@@ -1,15 +1,36 @@
 import * as cdk from 'aws-cdk-lib'
 import { PublicBucket } from './constructs/public-bucket'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns'
 
 export interface BackStackProps extends cdk.StackProps {
   stage: string
 }
 
 export class BackStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc
   constructor(scope: cdk.App, id: string, props: BackStackProps) {
     super(scope, id, props)
+    this.vpc = new ec2.Vpc(this, `VPC-${props?.stage}`, {
+      maxAzs: 2
+    })
+
     new PublicBucket(this, 'FomoPlayerPreviews', {
       bucketName: `fomoplayer-previews-${props.stage}`
     })
+
+    const cluster = new ecs.Cluster(this, `FomoPlayerCluster-${props.stage}`, { vpc: this.vpc })
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      `FomoPlayerFargate-${props.stage}`,
+      {
+        cluster,
+        listenerPort: 3000,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromAsset(__dirname + '../../back')
+        }
+      }
+    )
   }
 }
