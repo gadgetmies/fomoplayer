6afe57495beb3585b7f77ab6c3e89745a11c4292
Gadgetmies <gadgetmies@gmail.com>
2023-05-02 19:12:33 +0300

    Remove shared dependency to enable Docker build

--- packages/back/config.js
+++ packages/back/config.js
@@ -1,12 +1,16 @@
 const nodeEnv = process.env.NODE_ENV || 'development'
 require('dotenv').config({ path: `.env.${nodeEnv}` })
 
-const resolveServiceURL = require('../shared/resolveServiceURL.js')
-const sharedConfig = require('multi-store-player-shared-config')(nodeEnv).config
+//const resolveServiceURL = require('../shared/resolveServiceURL.js')
+const sharedConfig = {
+  FRONTEND_URL: 'https://d3lojmgbaazgkr.cloudfront.net',
+  API_URL: 'http://backs-fomop-ntxv1350b6v3-1616753071.eu-north-1.elb.amazonaws.com:3000'
+}
+// require('multi-store-player-shared-config')(nodeEnv).config
 
 const port = sharedConfig.API_PORT
-const frontendURL = resolveServiceURL(sharedConfig.FRONTEND_URL, sharedConfig.IP, sharedConfig.FRONTEND_PORT)
-const apiURL = resolveServiceURL(sharedConfig.API_URL, sharedConfig.IP, port, '/api')
+const frontendURL = 'https://d3lojmgbaazgkr.cloudfront.net' //resolveServiceURL(sharedConfig.FRONTEND_URL, sharedConfig.IP, sharedConfig.FRONTEND_PORT)
+const apiURL = 'http://backs-fomop-ntxv1350b6v3-1616753071.eu-north-1.elb.amazonaws.com:3000' //resolveServiceURL(sharedConfig.API_URL, sharedConfig.IP, port, '/api')
 
 module.exports = {
   allowedOrigins: [frontendURL, 'chrome-extension://biafmljflmgpbaghhebhmapgajdkdahn'],
