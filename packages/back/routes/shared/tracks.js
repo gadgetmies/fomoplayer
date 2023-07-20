65b9a8b686de2b8dfa5374dfbf5bf9cb971cd619
Gadgetmies <gadgetmies@gmail.com>
2023-07-20 15:09:11 +0300

    Remove unhelpful logging

--- packages/back/routes/shared/tracks.js
+++ packages/back/routes/shared/tracks.js
@@ -143,7 +143,6 @@ module.exports.updateArtistTracks = async (storeUrl, details, sourceId) => {
   const storeModule = getStoreModule(storeUrl)
   const users = await getUsersFollowingArtist(details.storeArtistId)
   const generator = await storeModule.logic.getArtistTracks(details)
-  logger.debug('Got Beatport tracks', { generator })
   let combinedErrors = []
 
   logger.debug(`Processing tracks for artist: ${details.url}`)
