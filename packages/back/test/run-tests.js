6a03b3c8d5c282a5b38ab9bd7ce07bae8b1b3918
Gadgetmies <gadgetmies@gmail.com>
2023-12-16 23:02:32 +0200

    Improve help in run-tests.js options;
    Previously the help did not mention that the files are searched
    recursively.

--- packages/back/test/run-tests.js
+++ packages/back/test/run-tests.js
@@ -46,25 +46,29 @@ const main = async (path, regex = /\.js/) => {
 
 const argv = yargs
   .usage('Usage: $0 <path> [options]')
-  .command('$0 <path>', 'Runs tests in path filtered by regex if given', yargs => {
-    yargs.positional('path', {
-      description: 'Regex to filter files with',
-      type: 'string'
-    })
-    yargs.option('regex', {
-      description: 'Regex to filter files with',
-      alias: 'r',
-      type: 'string'
-    })
-    yargs.check(argv => {
-      if (R.isEmpty(argv.regex)) {
-        throw new Error('regex option used, but regex string was empty!')
-      }
-      return true
-    })
-  }, async argv => {
-    return await main(argv.path, argv.regex)
-  })
+  .command(
+    '$0 <path>',
+    'Runs tests in path filtered by regex if given',
+    yargs => {
+      yargs.positional('path', {
+        description: 'Path to test files. Searched recursively.',
+        type: 'string'
+      })
+      yargs.option('regex', {
+        description: 'Regex to filter files with',
+        alias: 'r',
+        type: 'string'
+      })
+      yargs.check(argv => {
+        if (R.isEmpty(argv.regex)) {
+          throw new Error('regex option used, but regex string was empty!')
+        }
+        return true
+      })
+    },
+    async argv => {
+      return await main(argv.path, argv.regex)
+    }
+  )
   .help()
-  .alias('help', 'h')
-  .argv
+  .alias('help', 'h').argv
