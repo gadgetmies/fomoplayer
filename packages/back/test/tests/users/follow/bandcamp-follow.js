390ae82e99d1a2fa9b652e84ba2e31457080a996
Gadgetmies <gadgetmies@gmail.com>
2023-12-20 00:29:09 +0200

    Implement tests for Bandcamp playlist follows

---
+++ packages/back/test/tests/users/follow/bandcamp-follow.js
@@ -0,0 +1,130 @@
+require('fomoplayer_shared').interceptors.spotify.init()
+const bandcampInterceptor = require('fomoplayer_shared').interceptors.bandcamp.init()
+
+const { waitUntil, TimeoutError } = require('async-wait-until')
+const R = require('ramda')
+
+const { initDb } = require('../../../lib/db')
+const assert = require('assert')
+const dnbPlaylistUrl = 'https://bandcamp.com/discover/electronic?tags=drum-bass'
+const dnbDigitalPlaylistUrl = 'https://bandcamp.com/discover/electronic/digital?tags=drum-bass'
+const electronicPlaylistUrl = 'https://bandcamp.com/discover/electronic'
+const electronicDigitalPlaylistUrl = 'https://bandcamp.com/discover/electronic/digital'
+const allDigitalPlaylistUrl = 'https://bandcamp.com/discover/all/digital'
+const {
+  static: { getTagsFromUrl }
+} = require('../../../../routes/stores/bandcamp/bandcamp-api.js')
+const { addPlaylistFollows } = require('../../../../routes/users/logic')
+const { test } = require('fomoplayer_shared/test/test')
+const pg = require('fomoplayer_shared').db.pg
+const sql = require('sql-template-strings')
+
+const deletePlaylist = async followDetails => {
+  assert.equal(followDetails.length, 1)
+  const match = followDetails[0].playlist.match(/.*\/(.*)$/)
+  const playlistId = match[1]
+  assert.ok(Number.isInteger(Number(playlistId)))
+  await pg.queryAsync(sql`DELETE FROM playlist WHERE playlist_id = ${playlistId}`)
+}
+
+const addFollowTeardown = async (...args) => {
+  await deletePlaylist(...args)
+  bandcampInterceptor.clearMockedRequests()
+}
+
+const waitUntilEquals = async (fn, expected, { timeout = 5000 } = {}) => {
+  try {
+    await waitUntil(() => R.equals(fn(), expected), { timeout })
+  } catch (e) {
+    if (e instanceof TimeoutError) {
+      throw new Error(
+        `Timeout (${timeout}ms) waiting for value to equal: ${JSON.stringify(
+          { expected, latest: await fn() },
+          null,
+          2
+        )}`
+      )
+    } else {
+      throw e
+    }
+  }
+}
+
+const waitUntilMockedRequestUrlsEqual = async (expected, { timeout = 4000 } = {}) =>
+  waitUntilEquals(
+    () => {
+      console.log('wait hallo')
+      return bandcampInterceptor.getMockedRequests().map(R.prop('url'))
+    },
+    expected,
+    { timeout }
+  )
+
+// TODO: ensure updatePlaylistTracks is called when adding follows
+const assertPlaylistIsAddedCorrectly = url => async () => {
+  console.log('hallo')
+  const details = await pg.queryRowsAsync(
+    'SELECT playlist_store_id, store_playlist_type_label FROM playlist NATURAL JOIN store_playlist_type'
+  )
+  assert.deepEqual(details, [
+    {
+      playlist_store_id: url,
+      store_playlist_type_label: 'Tag'
+    }
+  ])
+
+  await waitUntilMockedRequestUrlsEqual([url])
+}
+const playlistFollowTest = url => ({
+  setup: async () => {
+    return await addPlaylistFollows([{ url }], 1)
+  },
+  'are added correctly': assertPlaylistIsAddedCorrectly(url),
+  teardown: addFollowTeardown
+})
+
+test({
+  setup: async () => {
+    await initDb()
+  },
+  'playlist urls are parsed correctly': {
+    'drum & bass playlist': () => {
+      assert.deepEqual(getTagsFromUrl(dnbPlaylistUrl), {
+        genre: 'electronic',
+        subgenre: 'drum-bass',
+        format: undefined
+      })
+    },
+    'digital drum & bass playlist': () => {
+      assert.deepEqual(getTagsFromUrl(dnbDigitalPlaylistUrl), {
+        genre: 'electronic',
+        subgenre: 'drum-bass',
+        format: 'digital'
+      })
+    },
+    'electronic playlist': () => {
+      assert.deepEqual(getTagsFromUrl(electronicPlaylistUrl), {
+        genre: 'electronic',
+        subgenre: undefined,
+        format: undefined
+      })
+    },
+    'digital electronic playlist': () => {
+      assert.deepEqual(getTagsFromUrl(electronicDigitalPlaylistUrl), {
+        genre: 'electronic',
+        format: 'digital',
+        subgenre: undefined
+      })
+    },
+    'digital playlist': () => {
+      assert.deepEqual(getTagsFromUrl(allDigitalPlaylistUrl), { format: 'digital', genre: 'all', subgenre: undefined })
+    }
+  },
+  'when a user follows a Bandcamp playlist': {
+    'genre follows': playlistFollowTest(electronicPlaylistUrl),
+    'subgenre follows': playlistFollowTest(dnbPlaylistUrl),
+    'subgenre format follows': playlistFollowTest(dnbDigitalPlaylistUrl),
+    'genre format follows': playlistFollowTest(electronicDigitalPlaylistUrl),
+    'format follows': playlistFollowTest(allDigitalPlaylistUrl)
+  }
+})
--- packages/shared/interceptors/bandcampInterceptor.js
+++ packages/shared/interceptors/bandcampInterceptor.js
@@ -6,21 +6,63 @@ const path = require('path')
 const bandcampNoisiaSearchMock = require('./fixtures/bandcamp-noisia-search.json')
 const bandcampNoisiaMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-noisia.html`)).toString('utf-8')
 const bandcampVisionMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-vision.html`)).toString('utf-8')
-const bandcampF4kY00Mock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-f4k-y00.html`)).toString(
-  'utf-8'
-)
+const bandcampF4kY00Mock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-f4k-y00.html`)).toString('utf-8')
 const bandcampDifferentEyesMock = readFileSync(
   path.resolve(`${__dirname}/fixtures/bandcamp-different-eyes.html`)
 ).toString('utf-8')
-const bandcampResonanceVMock = readFileSync(
-  path.resolve(`${__dirname}/fixtures/bandcamp-resonance-v.html`)
+const bandcampResonanceVMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-resonance-v.html`)).toString(
+  'utf-8'
+)
+const bandcampResonanceVIMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-resonance-vi.html`)).toString(
+  'utf-8'
+)
+
+const bandcampElectronicMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-electronic.html`)).toString(
+  'utf-8'
+)
+const bandcampAllMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-all.html`)).toString('utf-8')
+const bandcampAllDigitalMock = readFileSync(path.resolve(`${__dirname}/fixtures/bandcamp-all-digital.html`)).toString(
+  'utf-8'
+)
+const bandcampElectronicDigitalMock = readFileSync(
+  path.resolve(`${__dirname}/fixtures/bandcamp-electronic-digital.html`)
 ).toString('utf-8')
-const bandcampResonanceVIMock = readFileSync(
-  path.resolve(`${__dirname}/fixtures/bandcamp-resonance-vi.html`)
+const bandcampElectronicDnbMock = readFileSync(
+  path.resolve(`${__dirname}/fixtures/bandcamp-electronic-drum-and-bass.html`)
+).toString('utf-8')
+const bandcampElectronicDigitalDnbMock = readFileSync(
+  path.resolve(`${__dirname}/fixtures/bandcamp-electronic-digital-drum-and-bass.html`)
 ).toString('utf-8')
 
 const bandcampRedirect = process.env.BANDCAMP_API_REDIRECT
 const useMocks = process.env.BANDCAMP_API_MOCK
+
+const respondWith = (body, contentType = 'application/json') => () => ({
+  body,
+  options: {
+    headers: {
+      'content-type': contentType
+    }
+  }
+})
+
+const respondWithHTML = html => respondWith(html, 'text/html; charset=utf-8')
+
+const HTMLmock = ([urlToMatch, html]) => ({
+  test: ({ url }) => url === urlToMatch,
+  getResponse: respondWithHTML(html)
+})
+
+const JSONmock = ([urlToMatch, json]) => ({
+  test: ({ url }) => url === urlToMatch,
+  getResponse: respondWith(json)
+})
+
+const getMocks = urlResponsePairs =>
+  urlResponsePairs.map(([matcher, response]) => ({
+    test: matcher instanceof Function ? matcher : ({ url: urlToMatch }) => matcher === urlToMatch,
+    getResponse: respondWith(response, response instanceof Object ? 'application/json' : 'text/html; charset=utf-8')
+  }))
 
 module.exports.init = () =>
   interceptor.init({
@@ -30,86 +72,24 @@ module.exports.init = () =>
         url: ({ url }) => (new URL(url).host = bandcampRedirect)
       }
     ],
-    mocks: [
-      {
-        test: ({ pathname }) => pathname === '/api/bcsearch_public_api/1/autocomplete_elastic',
-        getResponse: () => ({
-          body: bandcampNoisiaSearchMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) => url === 'https://noisia.bandcamp.com/music',
-        getResponse: () => ({
-          body: bandcampNoisiaMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) => url === 'https://noisia.bandcamp.com/album/the-resonance-vi',
-        getResponse: () => ({
-          body: bandcampResonanceVIMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) => url === 'https://noisia.bandcamp.com/album/the-resonance-v',
-        getResponse: () => ({
-          body: bandcampResonanceVMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) => url === 'https://visionrecordings.bandcamp.com/music',
-        getResponse: () => ({
-          body: bandcampVisionMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) => url === 'https://billainaethek.bandcamp.com/album/f4k-y00?label=4289352950&tab=music',
-        getResponse: () => ({
-          body: bandcampF4kY00Mock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      },
-      {
-        test: ({ url }) =>
-          url === 'https://billainaethek.bandcamp.com/album/different-eyes-ep?label=4289352950&tab=music',
-        getResponse: () => ({
-          body: bandcampDifferentEyesMock,
-          options: {
-            headers: {
-              'content-type': 'text/html; charset=utf-8'
-            }
-          }
-        })
-      }
-    ],
+    mocks: getMocks([
+      [({ pathname }) => pathname === '/api/bcsearch_public_api/1/autocomplete_elastic', bandcampNoisiaSearchMock],
+      ['https://noisia.bandcamp.com/music', bandcampNoisiaMock],
+      ['https://noisia.bandcamp.com/album/the-resonance-vi', bandcampResonanceVIMock],
+      ['https://noisia.bandcamp.com/album/the-resonance-v', bandcampResonanceVMock],
+      ['https://visionrecordings.bandcamp.com/music', bandcampVisionMock],
+      ['https://visionrecordings.bandcamp.com/album/f4k-y00label=4289352950&tab=music', bandcampF4kY00Mock],
+      [
+        'https://visionrecordings.bandcamp.com/album/different-eyes-ep?label=4289352950&tab=music',
+        bandcampDifferentEyesMock
+      ],
+      ['https://bandcamp.com/discover', bandcampAllMock],
+      ['https://bandcamp.com/discover/all/digital', bandcampAllDigitalMock],
+      ['https://bandcamp.com/discover/electronic', bandcampElectronicMock],
+      ['https://bandcamp.com/discover/electronic/digital', bandcampElectronicDigitalMock],
+      ['https://bandcamp.com/discover/electronic?tags=drum-bass', bandcampElectronicDnbMock],
+      ['https://bandcamp.com/discover/electronic/digital?tags=drum-bass', bandcampElectronicDigitalDnbMock]
+    ]),
     name: 'Bandcamp',
     regex: BandcampUrlRegex,
     bandcampNoisiaSearchMock: bandcampNoisiaSearchMock,
