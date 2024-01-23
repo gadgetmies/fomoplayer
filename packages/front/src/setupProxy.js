24e470f78bde7b00ff45f216012f382504423754
Gadgetmies <gadgetmies@gmail.com>
2024-01-23 22:48:33 +0200

    Implement support for debug on mobile devices;
    Google (and probably Spotify) cannot be configured to allow ip addresses
    for return urls. Also https is required, so the easiest solution is to
    use a tunneling service like ngrok. Ngrok however does not support
    redirecting different paths to different ports, so a frontend proxy
    (or alternatively a backend proxy / serving of front) is required.
    
    In order to start the frontend for debugging, add a domain in Ngrok UI
    and run:
    
    > API_URL=https://your_public_url.ngrok-free.app/api\
    FRONTEND_URL=https://your_public_url.ngrok-free.app\
    yarn start
    
    > ngrok http --domain=your_public_url.ngrok-free.app 4004

---
+++ packages/front/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const config = require('fomoplayer_shared').config(process.env.NODE_ENV).config
+
+module.exports = function(app) {
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:4003', // TODO: fix: config.API_URL,
+        changeOrigin: true
+      })
+    )
+  }
+}
