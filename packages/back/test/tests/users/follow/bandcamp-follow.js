b06a654227105de8619d717e8719fb1458b2d5df
Gadgetmies <gadgetmies@gmail.com>
2024-05-15 15:36:33 +0300

    Reformat

--- packages/back/test/tests/users/follow/bandcamp-follow.js
+++ packages/back/test/tests/users/follow/bandcamp-follow.js
@@ -12,14 +12,14 @@ const electronicPlaylistUrl = 'https://bandcamp.com/discover/electronic'
 const electronicDigitalPlaylistUrl = 'https://bandcamp.com/discover/electronic/digital'
 const allDigitalPlaylistUrl = 'https://bandcamp.com/discover/all/digital'
 const {
-  static: { getTagsFromUrl }
+  static: { getTagsFromUrl },
 } = require('../../../../routes/stores/bandcamp/bandcamp-api.js')
 const { addPlaylistFollows } = require('../../../../routes/users/logic')
 const { test } = require('fomoplayer_shared/test/test')
 const pg = require('fomoplayer_shared').db.pg
 const sql = require('sql-template-strings')
 
-const deletePlaylist = async followDetails => {
+const deletePlaylist = async (followDetails) => {
   assert.equal(followDetails.length, 1)
   const match = followDetails[0].playlist.match(/.*\/(.*)$/)
   const playlistId = match[1]
@@ -41,8 +41,8 @@ const waitUntilEquals = async (fn, expected, { timeout = 5000 } = {}) => {
         `Timeout (${timeout}ms) waiting for value to equal: ${JSON.stringify(
           { expected, latest: await fn() },
           null,
-          2
-        )}`
+          2,
+        )}`,
       )
     } else {
       throw e
@@ -51,36 +51,28 @@ const waitUntilEquals = async (fn, expected, { timeout = 5000 } = {}) => {
 }
 
 const waitUntilMockedRequestUrlsEqual = async (expected, { timeout = 4000 } = {}) =>
-  waitUntilEquals(
-    () => {
-      console.log('wait hallo')
-      return bandcampInterceptor.getMockedRequests().map(R.prop('url'))
-    },
-    expected,
-    { timeout }
-  )
+  waitUntilEquals(() => bandcampInterceptor.getMockedRequests().map(R.prop('url')), expected, { timeout })
 
 // TODO: ensure updatePlaylistTracks is called when adding follows
-const assertPlaylistIsAddedCorrectly = url => async () => {
-  console.log('hallo')
+const assertPlaylistIsAddedCorrectly = (url) => async () => {
   const details = await pg.queryRowsAsync(
-    'SELECT playlist_store_id, store_playlist_type_label FROM playlist NATURAL JOIN store_playlist_type'
+    'SELECT playlist_store_id, store_playlist_type_label FROM playlist NATURAL JOIN store_playlist_type',
   )
   assert.deepEqual(details, [
     {
       playlist_store_id: url,
-      store_playlist_type_label: 'Tag'
-    }
+      store_playlist_type_label: 'Tag',
+    },
   ])
 
   await waitUntilMockedRequestUrlsEqual([url])
 }
-const playlistFollowTest = url => ({
+const playlistFollowTest = (url) => ({
   setup: async () => {
     return await addPlaylistFollows([{ url }], 1)
   },
   'are added correctly': assertPlaylistIsAddedCorrectly(url),
-  teardown: addFollowTeardown
+  teardown: addFollowTeardown,
 })
 
 test({
@@ -92,39 +84,39 @@ test({
       assert.deepEqual(getTagsFromUrl(dnbPlaylistUrl), {
         genre: 'electronic',
         subgenre: 'drum-bass',
-        format: undefined
+        format: undefined,
       })
     },
     'digital drum & bass playlist': () => {
       assert.deepEqual(getTagsFromUrl(dnbDigitalPlaylistUrl), {
         genre: 'electronic',
         subgenre: 'drum-bass',
-        format: 'digital'
+        format: 'digital',
       })
     },
     'electronic playlist': () => {
       assert.deepEqual(getTagsFromUrl(electronicPlaylistUrl), {
         genre: 'electronic',
         subgenre: undefined,
-        format: undefined
+        format: undefined,
       })
     },
     'digital electronic playlist': () => {
       assert.deepEqual(getTagsFromUrl(electronicDigitalPlaylistUrl), {
         genre: 'electronic',
         format: 'digital',
-        subgenre: undefined
+        subgenre: undefined,
       })
     },
     'digital playlist': () => {
       assert.deepEqual(getTagsFromUrl(allDigitalPlaylistUrl), { format: 'digital', genre: 'all', subgenre: undefined })
-    }
+    },
   },
   'when a user follows a Bandcamp playlist': {
     'genre follows': playlistFollowTest(electronicPlaylistUrl),
     'subgenre follows': playlistFollowTest(dnbPlaylistUrl),
     'subgenre format follows': playlistFollowTest(dnbDigitalPlaylistUrl),
     'genre format follows': playlistFollowTest(electronicDigitalPlaylistUrl),
-    'format follows': playlistFollowTest(allDigitalPlaylistUrl)
-  }
+    'format follows': playlistFollowTest(allDigitalPlaylistUrl),
+  },
 })
