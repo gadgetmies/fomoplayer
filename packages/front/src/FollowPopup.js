d2e385bb57bb748921cfacaed409921fda28a296
Gadgetmies <gadgetmies@gmail.com>
2025-01-20 23:56:47 +0200

    Implement follow all artists / labels

--- packages/front/src/FollowPopup.js
+++ packages/front/src/FollowPopup.js
@@ -16,6 +16,8 @@ class FollowPopup extends Component {
     this.state = {
       subscribingToStoreLabel: null,
       subscribingToStoreArtist: null,
+      subscribingToAllStoreLabels: false,
+      subscribingToStoreArtists: false,
       refreshingList: false,
     }
   }
@@ -45,6 +47,20 @@ class FollowPopup extends Component {
       return null
     }
 
+    const artistsNotFollowed = this.state.artistDetails?.flatMap((artist) =>
+      artist.stores
+        .filter(R.prop('url'))
+        .filter(({ id }) => !this.getFollowingArtist(id))
+        .map(({ url, id: storeId }) => ({ url, storeId, name: artist.name })),
+    )
+
+    const labelsNotFollowed = this.state.labelDetails?.flatMap((label) =>
+      label.stores
+        .filter(R.prop('url'))
+        .filter(({ id }) => !this.getFollowingLabel(id))
+        .map(({ url, id: storeId }) => ({ url, storeId, name: label.name })),
+    )
+
     return (
       <FullScreenPopup title="Follow / unfollow" {...this.props}>
         <h2>Artists</h2>
@@ -66,14 +82,14 @@ class FollowPopup extends Component {
                     url={url}
                     following={following}
                     loading={subscribing}
-                    disabled={this.state.subscribingToStoreArtist}
+                    disabled={this.state.subscribingToStoreArtist || this.state.subscribingToAllStoreArtists}
                     onClick={async () => {
                       this.setState({ subscribingToStoreArtist: id })
 
                       try {
                         await this.props.onFollowStoreArtist(id, url, artist.name, !following)
                       } catch (e) {
-                        console.error('Ignoring store artist failed', e)
+                        console.error('Following store artist failed', e)
                         throw e
                       }
 
@@ -84,6 +100,29 @@ class FollowPopup extends Component {
               })
             })
           )}
+        </div>
+        <div style={{ paddingTop: '16px' }}>
+          <SpinnerButton
+            icon={'heart'}
+            size="large"
+            loading={this.state.subscribingToAllStoreArtists}
+            disabled={
+              this.state.subscribingToAllStoreArtists ||
+              artistsNotFollowed?.length === 0 ||
+              this.state.subscribingToStoreArtist
+            }
+            onClick={async () => {
+              this.setState({ subscribingToAllStoreArtists: true })
+              await Promise.all(
+                artistsNotFollowed.map(({ url, storeId, name }) =>
+                  this.props.onFollowStoreArtist(storeId, url, name, true),
+                ),
+              )
+              this.setState({ subscribingToAllStoreArtists: false })
+            }}
+          >
+            Follow all artists on all stores
+          </SpinnerButton>
         </div>
         {this.state.labelDetails?.length === 0 ? null : (
           <>
@@ -105,14 +144,15 @@ class FollowPopup extends Component {
                         url={url}
                         following={following}
                         loading={this.state.subscribingToStoreLabel === id}
-                        disabled={this.state.subscribingToStoreLabel}
+                        disabled={this.state.subscribingToStoreLabel || this.state.subscribingToAllStoreLabels}
                         onClick={async () => {
-                          this.setState({ subscribingToStoreLabel: id })
+                          this.setState({ subscribingToAllStoreLabel: id })
+                          const following = this.getFollowingLabel(id)
 
                           try {
                             await this.props.onFollowStoreLabel(id, url, label.name, !following)
                           } catch (e) {
-                            console.error('Ignoring store artist failed', e)
+                            console.error('Following store artist failed', e)
                             throw e
                           }
 
@@ -123,6 +163,29 @@ class FollowPopup extends Component {
                   })
                 })
               )}
+            </div>
+            <div style={{ paddingTop: '16px' }}>
+              <SpinnerButton
+                icon={'heart'}
+                size="large"
+                loading={this.state.subscribingToAllStoreLabels}
+                disabled={
+                  this.state.subscribingToAllStoreLabels ||
+                  labelsNotFollowed?.length === 0 ||
+                  this.state.subscribingToStoreLabel
+                }
+                onClick={async () => {
+                  this.setState({ subscribingToAllStoreLabels: true })
+                  await Promise.all(
+                    labelsNotFollowed.map(({ url, storeId, name }) =>
+                      this.props.onFollowStoreLabel(storeId, url, name, true),
+                    ),
+                  )
+                  this.setState({ subscribingToAllStoreLabels: false })
+                }}
+              >
+                Follow all artists on all stores
+              </SpinnerButton>
             </div>
           </>
         )}
