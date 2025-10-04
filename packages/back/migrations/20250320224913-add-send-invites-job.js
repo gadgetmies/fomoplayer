33dfd8dd3ed4f5158b52db635dbec1830f5526ac
Gadgetmies <gadgetmies@gmail.com>
2025-10-04 18:00:15 +0300

    Silence db migrations

--- packages/back/migrations/20250320224913-add-send-invites-job.js
+++ packages/back/migrations/20250320224913-add-send-invites-job.js
@@ -40,7 +40,9 @@ exports.down = function(db) {
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
-      console.log('received data: ' + data);
+      if (db.log.isSilent !== true) {
+        console.log('received data: ' + data)
+      }
 
       resolve(data);
     });
